c4493fa6a676b8d2a6e3286e659e69ce8a3f2e99
egifrediansyah <panjidwinanto4@gmail.com>
2019-05-11 16:02:38 +0700

    chatan

---
+++ app/src/main/java/founder/panji/model/Friend.kt
@@ -0,0 +1,11 @@
+package founder.panji.myapplication.model
+
+import founder.panji.myapplication.data.ParseFirebaseData
+
+import java.io.Serializable
+
+/**
+ *   created by Irfan Assidiq on 2019-05-11
+ *   email : assidiq.irfan@gmail.com
+ **/
+class Friend(val id: String, val name: String, val photo: String) : Serializable
